cef828bea5035fb052ebfeb310f7ff726d559d9a
AndikaNugraha11 <m.andika.nugraha@gmail.com>
2017-03-07 00:07:22 +0700

    Express setup & Routing

---
+++ app.js
@@ -0,0 +1,47 @@
+const express		= require('express');
+const path			= require('path');
+const bodyParser	= require('body-parser');
+const cors			= require('cors');
+const passport		= require('passport');
+const mongoose		= require('mongoose');
+const app			= express();
+const config		= require('./config/database');
+
+//Database configuration
+mongoose.connect(config.database);
+
+//On connect Database
+mongoose.connection.on('connected', ()=>{
+	console.log(`database terhubung dengan ${config.database}`);
+});
+
+//On Error database
+mongoose.connection.on('error', (err)=>{
+	console.log(`Error : ${err}`);
+});
+
+//Port Number
+const port = 3000;
+
+//CORS Middleware
+app.use(cors());
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Users route
+const users = require('./routes/users');
+app.use('/users', users);
+
+//Index Route
+app.get('/', (req,res)=> {
+	res.send('Hello PI');
+});
+
+//Start Server
+app.listen(port, ()=> {
+	console.log(`Server berjalan pada port ${port}`);
+})
---
+++ routes/users.js
@@ -0,0 +1,25 @@
+const express	= require('express');
+const router	= express.Router();
+
+
+//Users/register
+router.get('/register', (req,res,next)=>{
+	res.send('register');
+});
+
+//Users/profile
+router.get('/profile', (req,res,next)=>{
+	res.send('profile');
+});
+
+//Users/Authenticate
+router.get('/authenticate', (req,res,next)=>{
+	res.send('authenticate');
+});
+
+//Users/validate
+router.get('/validate', (req,res,next)=>{
+	res.send('validate');
+});
+
+module.exports = router;
