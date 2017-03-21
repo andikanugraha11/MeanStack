7fe6edd70730a20a52d083028e468ef2d62ed207
AndikaNugraha11 <m.andika.nugraha@gmail.com>
2017-03-21 22:19:45 +0700

    Register page angular

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ValidateService} from '../../services/validate.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+	nama		: String;
+	username 	: String;
+	email		: String;
+	password	: String;
 
-  ngOnInit() {
-  }
+	constructor(private validateService : ValidateService) { }
+
+	ngOnInit() {
+
+	}
+
+	onRegisterSubmit(){
+		const user = {
+			nama		: this.nama,
+			username	: this.username,
+			email		: this.email,
+			password	: this.password
+		}
+
+		//required
+		if(!this.validateService.validateRegister(user)){
+			console.log('isi semua');
+			return false;
+		}
+
+		if(!this.validateService.validateEmail(user.email)){
+			console.log('email salah');
+			return false;
+		}
+	}
 
 }
